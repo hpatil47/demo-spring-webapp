3452507336dd147246356facf04736e7c9eeaa30
hpatil47 <49780454+hpatil47@users.noreply.github.com>
2021-08-20 12:24:59 +0530

    adding spring boot jar app

---
+++ src/main/java/com/example/demohelloapp/DemohelloappApplication.java
@@ -0,0 +1,13 @@
+package com.example.demohelloapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohelloappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemohelloappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demohelloapp/DemohelloappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demohelloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemohelloappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
