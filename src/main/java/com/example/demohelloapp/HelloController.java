3ccacf6950670b369d726369cd274be1f3c80a1f
hpatil47 <49780454+hpatil47@users.noreply.github.com>
2021-08-31 16:44:39 +0530

    Update HelloController.java

--- src/main/java/com/example/demohelloapp/HelloController.java
+++ src/main/java/com/example/demohelloapp/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World from Spring Boot!";
+        return "welcome to spring boot app running on azure web app!";
     }
 }
